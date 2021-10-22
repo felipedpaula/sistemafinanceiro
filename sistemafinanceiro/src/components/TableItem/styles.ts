9420adba61bd851e0330cfffb33abc204afc414b
Felipe Palmeira <felipedpaula97@gmail.com>
2021-10-22 13:28:25 -0300

    finalizando

--- sistemafinanceiro/src/components/TableItem/styles.ts
+++ sistemafinanceiro/src/components/TableItem/styles.ts
@@ -14,3 +14,7 @@ export const Category = styled.div<{ color: string }>`
     width: fit-content;
     background-color: ${props => props.color};
 `;
+
+export const Value = styled.div<{ color: string }>`
+    color: ${props => props.color};
+`;
--- sistemafinanceiro/src/helpers/dateFilter.ts
+++ sistemafinanceiro/src/helpers/dateFilter.ts
@@ -5,14 +5,14 @@ export const getCurrentMonth = () => {
     return `${now.getFullYear()}-${now.getMonth()+1}`;
 }
 
-export const filterListByMonth = (list:Item[], date: string): Item[] => {
+export const filterListByMonth = (list: Item[], date: string): Item[] => {
     let newList: Item[] = [];
     let [year, month] = date.split('-');
 
     for(let i in list) {
         if(
             list[i].date.getFullYear() === parseInt(year) &&
-            (list[i].date.getMonth()+1) === parseInt(month)
+            (list[i].date.getMonth() + 1) === parseInt(month)
         ) {
             newList.push(list[i]);
         }
@@ -22,11 +22,16 @@ export const filterListByMonth = (list:Item[], date: string): Item[] => {
 }
 
 export const formatDate = (date: Date): string => {
-    let day = date.getDate();
-    let month = date.getMonth()+1;
     let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
 
     return `${addZeroToDate(day)}/${addZeroToDate(month)}/${year}`;
 }
+const addZeroToDate = (n: number): string => n < 10 ? `0${n}` : `${n}`;
 
-const addZeroToDate = (n: number):string => n < 10 ? `0${n}` : `${n}`;
+export const formatCurrentMonth = (currentMonth: string): string => {
+    let [year, month] = currentMonth.split('-');
+    let months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+    return `${months[parseInt(month) - 1]} de ${year}`;
+}
